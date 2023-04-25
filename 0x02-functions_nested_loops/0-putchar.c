568d773243dc534a7999d454729ab73daf332fcb
sama essam <samaatwa99@gmailn.com>
2023-04-25 15:46:13 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include"main"
+/**
+ * main-print_putchar
+ * is witten by mbah
+ *  Return: 0
+  */
+
+
+	int main(void)
+	{
+	_putchar('_')
+        _putchar('p');
+	_putchar('u');
+	_putcharr('t');
+	_putchar('c');
+        _putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	_putchar (0);
+	}
